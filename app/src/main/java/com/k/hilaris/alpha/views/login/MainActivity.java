00a0aecc1c61a64983942cd58df4f0a095b706a1
Kevin Oh <kevinoh94@gmail.com>
2018-10-14 15:22:59 +0900

    update logo

--- app/src/main/java/com/k/hilaris/alpha/views/login/MainActivity.java
+++ app/src/main/java/com/k/hilaris/alpha/views/login/MainActivity.java
@@ -47,7 +47,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         logo = findViewById(R.id.logo);
-        Picasso.get().load(R.drawable.logo).resize(756, 588).into(logo);
+        //Picasso.get().load(R.drawable.logo).resize(756, 588).into(logo);
+        Picasso.get().load(R.drawable.logo).into(logo);
 
         google_sign_in = findViewById(R.id.google_sign_in);
         google_sign_in.setOnClickListener(this);
