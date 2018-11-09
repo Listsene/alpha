2157e4e1353317611b038909dd7fad5ca8ea42a9
Kevin Oh <kevinoh@ajou.ac.kr>
2018-11-09 18:40:15 +0900

    Adding input buttons to sudoku

---
+++ app/src/main/java/com/k/hilaris/alpha/views/sudoku/InputButtonsFragment.java
@@ -0,0 +1,21 @@
+package com.k.hilaris.alpha.views.sudoku;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.k.hilaris.alpha.R;
+
+public class InputButtonsFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout., container, false);
+}
