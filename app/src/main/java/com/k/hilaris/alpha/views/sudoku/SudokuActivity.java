242e8a89d0cdd3790bb81563d3c52bdc257f6302
Kevin Oh <kevinoh94@gmail.com>
2018-11-19 17:18:40 +0900

    Added timer to Sudoku

--- app/src/main/java/com/k/hilaris/alpha/views/sudoku/SudokuActivity.java
+++ app/src/main/java/com/k/hilaris/alpha/views/sudoku/SudokuActivity.java
@@ -3,14 +3,22 @@ package com.k.hilaris.alpha.views.sudoku;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.k.hilaris.alpha.R;
 import com.k.hilaris.alpha.models.Sudoku;
 
 import org.w3c.dom.Text;
 
+import java.util.concurrent.TimeUnit;
+
 public class SudokuActivity extends AppCompatActivity implements InputButtonsGridFragment.TextClicked {
+    private Toolbar mToolbar;
+    TextView timerTv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +29,38 @@ public class SudokuActivity extends AppCompatActivity implements InputButtonsGri
         ft.add(R.id.SudokuGridFragment, new SudokuGridFragment());
         ft.add(R.id.InputButtonsFragment, new InputButtonsGridFragment());
         ft.commit();
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Timer();
     }
+
+    long timeRemaining = 300000;
+
+    public boolean Timer(){
+        new CountDownTimer(timeRemaining, 1000) {
+            public void onTick(long millisUntilFinished) {
+                long millis = millisUntilFinished;
+                String hms;
+                if((TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis))<10)){
+                    hms =  ("0"+TimeUnit.MILLISECONDS.toHours(millis))+":0"+ (TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)))+":0"+ (TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+                }else{
+                    hms =  ("0"+TimeUnit.MILLISECONDS.toHours(millis))+":0"+ (TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)))+":"+ (TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+                }
+
+                timerTv = findViewById(R.id.timerTextView);
+                timerTv.setText(hms);
+                timeRemaining = millis;
+            }
+            public void onFinish() {
+                timerTv.setText("done!");
+            }
+        }.start();
+        return true;
+    }
+
 
     @Override
     public void sendText(String text){
