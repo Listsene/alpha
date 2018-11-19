58939002d707942b06ff48a882212d8dcfdc1d9c
goodgeon <gg@ggui-MacBook-Pro.local>
2018-11-19 20:46:54 +0900

    memo,clear,enter

--- app/src/main/java/com/k/hilaris/alpha/adapters/SudokuGridAdapter.java
+++ app/src/main/java/com/k/hilaris/alpha/adapters/SudokuGridAdapter.java
@@ -56,7 +56,7 @@ public class SudokuGridAdapter extends BaseAdapter {
         }
         else {
             cell.setText(number);
-            //cell.setEnabled(false);       //??
+            cell.setEnabled(false);       //??
         }
 
 
