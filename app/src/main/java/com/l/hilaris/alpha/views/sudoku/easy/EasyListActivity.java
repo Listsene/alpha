7227644d8def5380a2bc3837524fcd33a3f5c99f
living4574 <adolfd@naver.com>
2018-12-12 22:35:09 +0900

    multiplay menu modified

--- app/src/main/java/com/l/hilaris/alpha/views/sudoku/easy/EasyListActivity.java
+++ app/src/main/java/com/l/hilaris/alpha/views/sudoku/easy/EasyListActivity.java
@@ -12,7 +12,7 @@ public class EasyListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy_list);
-
+        
         String mode = (String) getIntent().getSerializableExtra("mode");
         getIntent().putExtra("mode", mode);
 
